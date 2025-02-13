4bd8392930e0754ffe2912058e773c62b7c10b6e
mac2274 <yuen.maggi@gmail.com>
2025-02-13 11:34:11 +0100

    complete first task

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 const image = document.querySelector('#img1');
-let 
+let counterDiv = document.querySelector('.counter-container');
+
+image.addEventListener('click', countNumber);
+let count = 0;
+
+function countNumber(){ 
+    let newCount = count++;
+
+    counterDiv.innerHTML = `
+    <label for="counter">Numbers hit</label>
+    <h1>${newCount}</h1>`;
+}
+
