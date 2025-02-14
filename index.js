fb149f34ea6ebef383336d5706570be41cb92b2b
mac2274 <yuen.maggi@gmail.com>
2025-02-14 08:18:19 +0100

    next level added but...

--- index.js
+++ index.js
@@ -3,12 +3,26 @@ let counterDiv = document.querySelector('.counter-container');
 
 image.addEventListener('click', countNumber);
 let count = 0;
+let nextLevel = 0;
+
 
 function countNumber(){ 
     let newCount = count++;
 
     counterDiv.innerHTML = `
-    <label for="counter">Numbers hit</label>
-    <h1>${newCount}</h1>`;
+        <label for="counter">Numbers hit</label>
+        <h2>${newCount}</h2>
+    `;
+
+    if(newCount>10 ){
+        let newLevel = nextLevel +1;
+        
+        counterDiv.innerHTML += `
+            <label for="counterLevel">Level</label>
+            <h2>${newLevel}</h2>
+        `;  
+    }
+
+ 
 }
 
