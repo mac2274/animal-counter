34263fff2ada5627a93fcd87c5322d97a9022cd2
mac2274 <yuen.maggi@gmail.com>
2025-02-14 12:06:06 +0100

    added change of image while reaching next level

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const image = document.querySelector('#img1');
+let image = document.querySelector('#img1');
 let counterDiv = document.querySelector('.counter-container');
 
 image.addEventListener('click', countNumber);
@@ -24,24 +24,34 @@ function insetHTML(){
 function levelCount(){
     if(count==10){
         nextLevel = nextLevel+1;
+        image.src = "resources/img/Unicorn/Unicorn_1.png";
     }else if(count==20){
         nextLevel = nextLevel+1;
+        image.src = "resources/img/Cat/Cat_1.png";
     }else if(count==30){
         nextLevel = nextLevel+1;
+        image.src = "resources/img/Dragon/Dragon_1.png";
     }else if(count==40){
         nextLevel = nextLevel+1;
+        image.src = "resources/img/Lizard/Lizard_1.png";
     }else if(count==50){
         nextLevel = nextLevel+1;
+        image.src = "resources/img/Octopus/Octopus_1.png";
     }else if(count==60){
         nextLevel = nextLevel+1;
+        image.src = "resources/img/Owl/Owl_1.png";
     }else if(count==70){
         nextLevel = nextLevel+1;
+        image.src = "resources/img/Pig/Pig_1.png";
     }else if(count==80){
         nextLevel = nextLevel+1;
+        image.src = "resources/img/Rainbowdog/Rainbowdog_1.png";
     }else if(count==90){
         nextLevel = nextLevel+1;
+        image.src = "resources/img/Sheep/Sheep_1.png";
     }else if(count==100){
         nextLevel = nextLevel+1;
+        image.src = "resources/img/Stag/Stag_1.png";
         }
 }
 
