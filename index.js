6722277a27f0ab0cf6445971bee0fae3b84aa6e3
mac2274 <yuen.maggi@gmail.com>
2025-02-19 09:25:45 +0100

    use the correct signs!

--- index.js
+++ index.js
@@ -2,6 +2,29 @@ let image = document.querySelector('#img1');
 let counterDiv = document.querySelector('.counter-container');
 let imgContainer = document.querySelector('.img-container');
 
+let arr1 = ['resources/img/Stag/Stag_1.png',
+    'resources/img/Unicorn/Unicorn_1.png',
+    'resources/img/Cat/Cat_1.png',
+    'resources/img/Dragon/Dragon_1.png',
+    'resources/img/Lizard/Lizard_1.png',
+    'resources/img/Octopus/Octopus_1.png',
+    'resources/img/Owl/Owl_1.png',
+    'resources/img/Pig/Pig_1.png',
+    'resources/img/Rainbowdog/Rainbowdog_1.png',
+    'resources/img/Sheep/Sheep_1.png'
+];
+
+let arr2 = ['resources/img/Stag/Stag_2.png',
+    'resources/img/Unicorn/Unicorn_2.png',
+    'resources/img/Cat/Cat_2.png',
+    'resources/img/Dragon/Dragon_2.png',
+    'resources/img/Lizard/Lizard_2.png',
+    'resources/img/Octopus/Octopus_2.png',
+    'resources/img/Owl/Owl_2.png',
+    'resources/img/Pig/Pig_2.png',
+    'resources/img/Rainbowdog/Rainbowdog_2.png',
+    'resources/img/Sheep/Sheep_2.png'
+];
 
 image.addEventListener('click', countNumber);
 let count = 0;
@@ -20,7 +43,7 @@ function insetHTML(){
     counterDiv.innerHTML = `
         <label for="counter">Hit</label>
         <h2>${count}</h2>
-         <label for="counter">Level</label>
+        <label for="counter">Level</label>
         <h2>${nextLevel}</h2>
     `;
 } 
@@ -61,35 +84,11 @@ function levelCount(){
 
 
 function changeImg(){
-    if(count%2==0){
-        imgContainer.innerHTML = `<img src="${arr2[0]}">`;
-    }else{
-        imgContainer.innerHTML = `<img src="${arr1[0]}">`;
-    }     
+    setTimeout(() => {
+        image.src = `${arr2[0]}`;
+    }, 200);
+    image.src = `${arr1[0]}`;
 }
 
-let arr1 = ['resources/img/Stag/Stag_1.png',
-    'resources/img/Unicorn/Unicorn_1.png',
-    'resources/img/Cat/Cat_1.png',
-    'resources/img/Dragon/Dragon_1.png',
-    'resources/img/Lizard/Lizard_1.png',
-    'resources/img/Octopus/Octopus_1.png',
-    'resources/img/Owl/Owl_1.png',
-    'resources/img/Pig/Pig_1.png',
-    'resources/img/Rainbowdog/Rainbowdog_1.png',
-    'resources/img/Sheep/Sheep_1.png'
-];
-
-let arr2 = ['resources/img/Stag/Stag_2.png',
-    'resources/img/Unicorn/Unicorn_2.png',
-    'resources/img/Cat/Cat_2.png',
-    'resources/img/Dragon/Dragon_2.png',
-    'resources/img/Lizard/Lizard_2.png',
-    'resources/img/Octopus/Octopus_2.png',
-    'resources/img/Owl/Owl_2.png',
-    'resources/img/Pig/Pig_2.png',
-    'resources/img/Rainbowdog/Rainbowdog_2.png',
-    'resources/img/Sheep/Sheep_2.png'
-];
 
 
